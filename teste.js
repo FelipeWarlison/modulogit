a8247dbea80c1b6bdb103386b3a547a073f49374
Felipe Warlison <felipewarlison@hotmail.com>
2019-07-18 20:12:56 -0300

    criando funções

---
+++ teste.js
@@ -0,0 +1,7 @@
+function trocarDiv(nome, idade)
+{
+	var area = document.getElementById("area");
+	var texto = prompt("Qual o seu sobrenome?");
+
+	area.innerHTML = nome+" "+texto+" tem "+idade+" anos";
+}
